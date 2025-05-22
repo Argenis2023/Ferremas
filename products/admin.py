328f84f3c39759c2d5477cb99a342ae2f0525209
ARGENIS RODRIGUEZ PRIETO <135769051+Argenis2023@users.noreply.github.com>
2025-05-22 15:34:40 -0400

    Registrar el modelo Product en el panel de administración y agregar la funcionalidad para manejar precios.

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Product, Price
 
-# Register your models here.
+class PriceInline(admin.TabularInline):
+    model = Price
+    extra = 1  # número de formularios extra para nuevos precios
+    min_num = 1
+    can_delete = True
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('code', 'brand', 'brand_code', 'name')
+    inlines = [PriceInline]
+    search_fields = ('code', 'brand', 'name')
